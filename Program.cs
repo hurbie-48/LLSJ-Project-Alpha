2715fb598547c51777f77807fe49eadb407e55b8
marison333 <marison.sol@outlook.com>
2026-09-10 12:41:50 +0200

    feat: created new monster.cs

--- Program.cs
+++ Program.cs
@@ -13,6 +13,19 @@ namespace LLSJProjectAlpha
             Console.WriteLine(player.name);
             Console.WriteLine($"Current health: {player.currentHitPoints}");
             Console.WriteLine($"Current health: {player.maximumHitPoints}");
+
+            // Test for monster class
+            Monster monster = new Monster();
+            monster.id = 1;
+            monster.name = "Rat";
+            monster.maximumDamage = 3;
+            monster.maximumHitPoints = 25;
+            monster.currentHitPoints = 25;
+
+            Console.WriteLine($"Monster: {monster.name}");
+            Console.WriteLine($"Damage: {monster.maximumDamage}");
+            Console.WriteLine($"Max health: {monster.maximumHitPoints}");
+            Console.WriteLine($"current health: {monster.currentHitPoints}");
         }
     }
 
