5bdf48c0285be9a6dececfaf0695fdda6a6d8777
Hamza <hberkane2000@gmail.com>
2026-09-16 11:04:30 +0200

    combat manager skeleton toegevoegd

---
+++ CombatManager.cs
@@ -0,0 +1,19 @@
+public static class CombatManager
+{
+    public const double HIT_CHANCE = 0.8;
+    public const double CRITICAL_HIT_CHANCE = 0.1;
+    public const double CRITICAL_HIT_MULTIPLIER = 1.5;
+    public const double DODGE_CHANCE_PER_LEVEL = 0.05;
+
+    public static void RunCombat(Player player, Monster monster)
+    {
+    }
+
+    public static void PlayerAttack(Player player, Monster monster)
+    {
+    }
+
+    public static void MonsterAttack(Player player, Monster monster)
+    {
+    }
+}
